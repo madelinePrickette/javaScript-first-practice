c49b5c35f7614b8579dcf693752eaf2def2ed92c
Madeline Prickette <madelineprickette@gmail.com>
2022-03-01 14:29:20 -0600

    practices incremental integers on plants and cats

--- bunnyCarrots.js
+++ bunnyCarrots.js
@@ -13,3 +13,22 @@ console.log( 'The amount of carrots this bunny ate was' , bunnyCarrots );
 bunnyCarrots = 'more carrots than yesterday.' ;
 
 console.log( 'The amount of carrots this bunny ate was' , bunnyCarrots );
+
+
+
+let plants = 12;
+//bought 3 plants
+plants = plants + 3;
+//cats ate 2 plants
+plants -= 2;
+//bought 1 new plant
+plants++;
+//next day bought 5 new plants
+plants+=5;
+//cats eat 1 plant
+plants--;
+console.log( plants );
+
+
+let motorcycles = 9;
+
