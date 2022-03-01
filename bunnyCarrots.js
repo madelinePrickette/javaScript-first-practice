cf3491f048353894683e60117348d8a9dfaf2e98
Madeline Prickette <madelineprickette@gmail.com>
2022-03-01 14:00:54 -0600

    wrote console logs including integer variables and string

--- bunnyCarrots.js
+++ bunnyCarrots.js
@@ -1,1 +1,15 @@
 console.log( 'js');
+
+let bunnyCarrots = 10;
+
+console.log( bunnyCarrots );
+
+console.log( 'The amount of carrots this bunny ate was' , bunnyCarrots );
+
+bunnyCarrots = 12;
+
+console.log( 'The amount of carrots this bunny ate was' , bunnyCarrots );
+
+bunnyCarrots = 'more carrots than yesterday.' ;
+
+console.log( 'The amount of carrots this bunny ate was' , bunnyCarrots );
